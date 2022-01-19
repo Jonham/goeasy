df1abab94ed0982fe4184452221d0621034b725b
jonham <jonham@qq.com>
2022-01-19 10:02:58 +0800

    feat: update SaveHTTPresourceAsFile

--- download/saveFile.go
+++ download/saveFile.go
@@ -4,15 +4,16 @@ import (
 	"io"
 	"net/http"
 	"os"
-
-	"github.com/Jonham/goeasy/checkerror"
 )
 
 // SaveHTTPResouceAsFile 下载文件，并保存到指定位置
-func SaveHTTPResouceAsFile(fileURL, filePath string) {
+func SaveHTTPResouceAsFile(fileURL, filePath string) error {
 	res, err := http.Get(fileURL)
-	checkerror.Check(err)
+	if err != nil {
+		return err
+	}
 
 	f, _ := os.Create(filePath)
-	io.Copy(f, res.Body)
+	_, err = io.Copy(f, res.Body)
+	return err
 }
